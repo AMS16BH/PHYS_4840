4332dfc449769ad7f887a78c42243e8f9a776fd1
Austin Smith <austins@cam.uwyo.edu>
2025-02-13 17:02:49 -0700

    Automated commit from Python Script.

--- 4. Function Library/git_function_lib.py
+++ 4. Function Library/git_function_lib.py
@@ -4,15 +4,18 @@ import subprocess
 import requests
 import os
 
-access_token = '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'
+access_token = input("Access_Token:")
 repo_path = '/d/users/austins/Desktop/PHYS_4840'
+github_username = "AMS16BH"
+github_repo = "PHYS_4840"
+COMMIT_MESSAGE = "Automated commit from Python Script."
+
 
 def authenticate_github(token):
     headers = {'Authorization': f'token {token}'}
     response = requests.get("https://api.github.com/user", headers=headers)
     if response.status_code == 200:
         print("Authentication successful!")
-        print(response.json())
     else:
         print(f"Authentication failed. Status code: {response.status_code}")
         print(response.text)
@@ -33,11 +36,14 @@ def run_command(command, cwd=None):
 
 def upload_to_github():
 
-    COMMIT_MESSAGE = "Automated commit from Python Script."
-
     if not os.path.exists(repo_path):
         print("Error: Repository path does not exist!")
         return
+
+    git_remote_url = f"https://{access_token}@github.com/{github_username}/{github_repo}.git"
+
+    run_command(f"git remote set-url origin {git_remote_url}", cwd=repo_path)
+
 
     run_command("git add .",cwd=repo_path)
     run_command(f'git commit -m "{COMMIT_MESSAGE}"',cwd=repo_path)
@@ -45,29 +51,3 @@ def upload_to_github():
 
 if __name__ == "__main__":
     upload_to_github()
-#def run_git_command(command):
-    #process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    #stdout, stderr = process.communicate()
-    #return stdout, stderr, process.returncode
-
-# Example usage:
-#command = "git status"
-#stdout, stderr, returncode = run_git_command(command)
-
-#if returncode == 0:
-    #print("Command executed successfully:")
-    #print(stdout)
-#else:
-    #print(f"Error executing command: {command}")
-    #print(stderr)
-
-# Example for pushing changes
-#command = "git push origin main"
-#stdout, stderr, returncode = run_git_command(command)
-
-#if returncode == 0:
-    #print("Command executed successfully:")
-    #print(stdout)
-#else:
-    #print(f"Error executing command: {command}")
-    #print(stderr)
